c245a620ce3da5f941376ad57d884308614aaad8
David Igón <david.igon@riseup.net>
2020-01-20 17:44:38 +0100

    add update and destroy functions to endpoint controller

--- app/controllers/decidim/comparative_stats/admin/endpoints_controller.rb
+++ app/controllers/decidim/comparative_stats/admin/endpoints_controller.rb
@@ -35,6 +35,39 @@ module Decidim
         def organization_endpoints
           @organization_endpoints ||= Endpoint.where(organization: current_organization)
         end
+
+        def update
+          enforce_permission_to :update, :endpoint, endpoint: current_endpoint
+
+          form = form(EndpointForm).from_params(params, endpoint: current_endpoint)
+
+          UpdateEndpoint.call(current_endpoint, form) do
+            on(:ok) do
+              flash[:notice] = I18n.t("endpoints.update.success", scope: "decidim.comparative_stats.admin")
+              redirect_to endpoints_path
+            end
+
+            on(:invalid) do
+              flash.now[:alert] = I18n.t("endpoints.update.error", scope: "decidim.comparative_stats.admin")
+              render :edit
+            end
+          end
+        end
+
+        def destroy
+          enforce_permission_to :destroy, :endpoint, endpoint: current_endpoint
+
+          DestroyEndpoint.call(current_endpoint, current_user) do
+            on(:ok) do
+              flash[:notice] = I18n.t("endpoints.destroy.success", scope: "decidim.comparative_stats.admin")
+              redirect_to endpoints_path
+            end
+          end
+        end
+
+        def current_endpoint
+          @current_endpoint ||= Endpoint.find(params[:id])
+        end
       end
     end
   end
