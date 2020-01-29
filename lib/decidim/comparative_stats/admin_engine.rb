d2dd8ccdd7da0451d434b8067f8eacf09c138549
David Igón <david.igon@riseup.net>
2020-01-29 10:02:57 +0100

    remove if allowed to to comparative stats admin menu

--- lib/decidim/comparative_stats/admin_engine.rb
+++ lib/decidim/comparative_stats/admin_engine.rb
@@ -41,8 +41,7 @@ module Decidim
                     decidim_admin_comparative_stats.endpoints_path,
                     icon_name: "graph",
                     position: 3.5,
-                    active: :inclusive,
-                    if: allowed_to?(:enter, :space_area, space_name: :comparative_stats)
+                    active: :inclusive
         end
       end
     end
