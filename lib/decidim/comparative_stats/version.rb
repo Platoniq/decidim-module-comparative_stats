fa7d87a63182fab8ad511e6d46d34bbe261c940a
Ivan Vergés <ivan@platoniq.net>
2021-01-13 23:23:43 +0100

    limit decidim version

--- lib/decidim/comparative_stats/version.rb
+++ lib/decidim/comparative_stats/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-module-comparative_stats version.
   module ComparativeStats
     VERSION = "1.0.1"
-    DECIDIM_VERSION = ">= 0.21.0"
+    DECIDIM_VERSION = [">= 0.21.0", "< 0.23.0"].freeze
     MIN_API_VERSION = "0.18.0"
   end
 end
