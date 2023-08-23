b6a55f58db22a65e00ae82262c02d161879c4481
Marina Muñoz <marinamuca01@gmail.com>
2023-08-23 13:20:01 +0200

    Update to 0.26 (#40)
    * Update to decidim 0.26 version
    
    * add gemfile.lock changes
    
    * Use latest 0.26 version
    
    * Use latest 0.26 version
    
    * Update .rubocop.yml
    
    * Fix development_app seed generation
    
    * Update version and README.md
    
    ---------
    
    Co-authored-by: Francisco Bolívar <francisco.bolivar@nazaries.com>

--- lib/decidim/comparative_stats/version.rb
+++ lib/decidim/comparative_stats/version.rb
@@ -4,7 +4,7 @@ module Decidim
   # This holds the decidim-module-comparative_stats version.
   module ComparativeStats
     VERSION = "1.2.0"
-    DECIDIM_VERSION = [">= 0.25.0", "< 0.26"].freeze
+    DECIDIM_VERSION = [">= 0.25.0", "< 0.27"].freeze
     MIN_API_VERSION = "0.18.0"
   end
 end
